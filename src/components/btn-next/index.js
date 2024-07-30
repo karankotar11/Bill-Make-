5f18d3e2ccb61634cda5df3aee972334c13733a1
karankotar11 <karankotar11@gmail.com>
2024-07-30 15:11:59 +0530

    Update index.js

--- src/components/btn-next/index.js
+++ src/components/btn-next/index.js
@@ -8,7 +8,7 @@ export default  function Btn({ setdisplayform, setdisplaydata,location }) {
             e.preventDefault();
             
             try {
-              const res = await fetch('/api/saveloc', {
+              const res = await fetch('/api', {
                 method: 'POST',
                 headers: {
                   'Content-Type': 'application/json',
