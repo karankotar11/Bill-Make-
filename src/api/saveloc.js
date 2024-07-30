2cea763f996c1ca7f88320174d0ec2ad4f31badd
karankotar11 <karankotar11@gmail.com>
2024-07-30 15:09:23 +0530

    Create saveloc.js

---
+++ src/api/saveloc.js
@@ -0,0 +1,19 @@
+
+import connectToDB from "@/database";
+import LocationofUser from "@/model"
+
+export default async function handler(req, res) {
+    if (req.method === "POST") {
+      await connectToDB();
+  
+      try {
+        const createdLocation = await LocationofUser.create(req.body.location);
+        res.status(200).json({ success: true, data: createdLocation });
+      } catch (e) {
+        console.error(e);
+        res.status(500).json({ success: false, error: e.message });
+      }
+    } else {
+      res.status(405).json({ success: false, error: "Method not allowed" });
+    }
+  }
