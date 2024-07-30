45afc6f085d529ad27309e9d71f08bbbd98fc5df
karankotar11 <karankotar11@gmail.com>
2024-07-30 14:26:42 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output:"standalone",
+    experimental: {
+        serverActions: true, // Enable server actions if you're using them
+      },
+};
 
 export default nextConfig;
